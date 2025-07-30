4a7769e46ef773b5335842ca090365b241ff6ab9
Raíssa <raissajoaquim29@gmail.com>
2025-07-29 21:37:32 -0300

    adicionando pratos e usando Intl.NumberFormat

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
 
 export default function Meals() {
     const [loadedMeals, setLoadedMeals] = useState([]);
@@ -19,6 +20,9 @@ export default function Meals() {
     }, []);
 
     return (
-        <ul id="meals">{loadedMeals.map(meal => <li key={meal.id}>{meal.name}</li>)}</ul>
+        <ul id="meals">{loadedMeals.map((meal) => (
+            <MealItem key={meal.id} meal={meal} />
+        ))}
+        </ul>
     )
 }
