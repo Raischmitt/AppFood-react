7b021c86f21a79d731ba3b7983eceec35015feb7
Raíssa <raissajoaquim29@gmail.com>
2025-08-04 21:41:30 -0300

    passando para form Actions

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useActionState } from 'react';
 
 import Modal from './UI/Modal';
 import Input from './UI/Input';
@@ -20,7 +20,12 @@ export default function Checkout() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
 
-    const { sendRequest, data, isLoading: isSending, error, clearData } = useHttp(
+    const {
+        sendRequest,
+        data,
+        error,
+        clearData
+    } = useHttp(
         'http://localhost:3000/orders',
         requestConfig
     );
@@ -40,13 +45,10 @@ export default function Checkout() {
         clearData();
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        const fd = new FormData(event.target);
+    async function checkoutAction(prevState, fd) {
         const customerData = Object.fromEntries(fd.entries());
 
-        sendRequest(
+        await sendRequest(
             JSON.stringify({
                 order: {
                     items: cartCtx.items,
@@ -55,6 +57,8 @@ export default function Checkout() {
             })
         );
     }
+
+    const [formState, formAction, isSending] = useActionState(checkoutAction, null);
 
     let actions = (
         <>
@@ -90,7 +94,7 @@ export default function Checkout() {
             open={userProgressCtx.progress === 'checkout'}
             onClose={handleClose}
         >
-            <form onSubmit={handleSubmit}>
+            <form action={formAction}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
